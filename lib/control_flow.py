c52a3e66e286ba645138a70bc0d3922acb48a584
Candy Bosibori Ongaro <candybosibori04@gmail.com>
2024-02-27 12:56:13 +0300

    done

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,58 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if username.upper() == "ADMIN" and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+    
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    # if betw 40 and 65
+    elif temperature >= 40 and temperature<= 65:
+    # return "It's a little chilly out there!"
+        return "It's a little chilly out there!"
+    # else if abv 85 rtn "It's too dang hot out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    # default "It's perfect out there!"
+    else:
+        return "It's perfect out there!"
+    
 
 def fizzbuzz(num):
-    # your code here
-    pass
+
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    
+    elif num % 3 == 0:
+        return "Fizz"
+
+    elif num % 5 == 0:
+        return "Buzz"
+    
+    else:
+        return num
+
+
+
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        if num2 != 0: 
+            return num1 / num2
+        else:
+            print("Cannot divide by zero!")
+            return None
+    else:
+        print("Invalid operation!")
+        return None
+
